c31b724fd5019b383fb0b859aa3cbfd8cc739ab0
Vlad Comanescu <alu0100968590@ull.edu.es>
2018-11-06 17:34:31 +0000

    crear el metodo initialize

--- lib/Etiqueta.rb
+++ lib/Etiqueta.rb
@@ -2,4 +2,15 @@ require "Etiqueta/version"
 
 class Etiqueta
 
+def initialize(nombre_etiqueta,grasas,grasas_saturadas,hidratos,azucares,proteinas,sal,dividido)
+    @nombre_etiqueta = String(nombre_etiqueta)
+    @grasas = Float(grasas)
+    @grasas_saturadas = Float(grasas_saturadas)
+    @hidratos = Float(hidratos)
+    @azucares = Float(azucares)
+    @proteinas = Float(proteinas)
+    @sal = Float(sal)
+    @dividido = Char(dividido)
+end
+
 end
